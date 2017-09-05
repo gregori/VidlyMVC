e75ab34cfb664b1335106cb4d867d8810b7a75d6
Rodrigo Gregori <spam@gregori.eti.br>
2017-09-05 15:55:33 -0300

    Adicionar Customers controller

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer() {Name = "José Ninguém", Id = 1},
+                new Customer() {Name = "João Qualquer", Id = 2}
+            };
+        }
+        
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
