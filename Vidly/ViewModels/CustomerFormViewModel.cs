e28e76c186a30f243d38c747982d9c6d9f47ee55
Rodrigo Gregori <spam@gregori.eti.br>
2017-10-03 21:45:15 -0300

    Adicionar botão de novo cliente e correção do título do formulário de criação/edição.

--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -10,6 +10,16 @@ namespace Vidly.ViewModels
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Customer != null && Customer.Id != 0)
+                    return "Editar Cliente";
+
+                return "Novo Cliente";
+            }
+        }
     }
 }
 
